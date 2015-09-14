0313e975c39e7efe916c28906e552c473b0052b8
Pedro Miguel Henriques Pereira <pmh.pereira@gmail.com>
2015-09-14 18:46:05 +0100

    new: text canvas to display information

---
+++ Assets/Scripts/CanvasController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CanvasController : MonoBehaviour {
+
+    public static CanvasController instance;
+
+    public Text textCanvas;
+
+    private PlayerController playerController;
+
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        instance = this;
+
+        playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+	}
+
+    void Update()
+    {
+        string textToDisplay = "";
+
+        textToDisplay += "stopOnCollision (C): " + playerController.stopOnCollision.ToString();
+        textToDisplay += "\n";
+        textToDisplay += "restart (R)";
+
+        textCanvas.text = textToDisplay;
+    }
+}
