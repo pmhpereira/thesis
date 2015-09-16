16e23c415a509973431e6dc2b0053c1895d68914
Pedro Miguel Henriques Pereira <pmh.pereira@gmail.com>
2015-09-16 11:18:24 +0100

    new: timer on screen

---
+++ Assets/Scripts/UI/Timer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class Timer : MonoBehaviour {
+
+    private Text textCanvas;
+    private float timer;
+
+    void Awake()
+    {
+        textCanvas = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        TimeSpan timeSpan = TimeSpan.FromSeconds(timer);
+        string timeText = string.Format("{0:D1}:{1:D2}.{2:D2}", (int) timeSpan.TotalMinutes, timeSpan.Seconds, timeSpan.Milliseconds);
+
+        textCanvas.text = timeText;
+    }
+}
