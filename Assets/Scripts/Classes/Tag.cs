8a0cebd717b546697b9e7d4c1594a09c30b26b8e
Pedro Miguel Henriques Pereira <pmh.pereira@gmail.com>
2015-11-23 14:56:23 +0000

    refactor: changed Tag class from enum to string

--- Assets/Scripts/Classes/Tag.cs
+++ Assets/Scripts/Classes/Tag.cs
@@ -1,13 +1,22 @@
-﻿public enum Tag
-{
-    None,
-    Jump,
-    Double_Jump,
-    Dash,
-    Slide
-}
+﻿using System.Collections.Generic;
 
-public static class TagMethods 
+public class Tag
 {
+    public static readonly string None = "None";
+    public static readonly string Jump = "Jump";
+    public static readonly string Double_Jump = "Double Jump";
 
+    public static readonly List<string> values = new List<string>(new string[] {
+        Tag.None,
+        Tag.Jump,
+        Tag.Double_Jump
+    });
+
+    public static void Add(string tag)
+    {
+        if(values.Contains(tag) == false)
+        {
+            values.Add(tag);
+        }
+    }
 }
