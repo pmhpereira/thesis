a1451f7fe99e40cb4282bf29753d3e1940d2430d
Pedro Miguel Henriques Pereira <pmh.pereira@gmail.com>
2016-02-02 17:23:16 +0000

    Added "Frustrated" mastery level

--- Assets/Scripts/Classes/Mastery.cs
+++ Assets/Scripts/Classes/Mastery.cs
@@ -41,28 +41,31 @@ public class MasteryComparison
 
 public class Mastery
 {
+    public static readonly string FRUSTRATED = "Frustrated";
     public static readonly string UNEXERCISED = "Unexercised";
     public static readonly string INITIATED = "Initiated";
-    public static readonly string BURNED_OUT = "Burned Out";
     public static readonly string PARTIALLY_MASTERED = "Partially Mastered";
     public static readonly string MASTERED = "Mastered";
+    public static readonly string BURNED_OUT = "Burned Out";
 
     public static readonly List<string> values = new List<string>(new string[] {
-        Mastery.BURNED_OUT,
+        Mastery.FRUSTRATED,
         Mastery.UNEXERCISED,
         Mastery.INITIATED,
         Mastery.PARTIALLY_MASTERED,
-        Mastery.MASTERED
+        Mastery.MASTERED,
+        Mastery.BURNED_OUT,
     });
 
     public static string FromId(string id)
     {
         switch(id) {
+            case "F": return FRUSTRATED;
             case "U": return UNEXERCISED;
             case "I": return INITIATED;
-            case "B": return BURNED_OUT;
             case "P": return PARTIALLY_MASTERED;
             case "M": return MASTERED;
+            case "B": return BURNED_OUT;
 
             default: return null;
         }
@@ -70,20 +73,22 @@ public class Mastery
 
     public static string ToId(string mastery)
     {
+        if (mastery == FRUSTRATED) return "F";
         if (mastery == UNEXERCISED) return "U";
         if (mastery == INITIATED) return "I";
-        if (mastery == BURNED_OUT) return "B";
         if (mastery == PARTIALLY_MASTERED) return "P";
         if (mastery == MASTERED) return "M";
+        if (mastery == BURNED_OUT) return "B";
 
         return null;
     }
 
     public static string FromAttempts(float score, int numAttempts)
     {
+        if (score == 1 && numAttempts >= 10) return BURNED_OUT;
         if (score >= 0.9 && numAttempts > 8) return MASTERED;
         if (score >= 0.5 && numAttempts > 6) return PARTIALLY_MASTERED;
-        if (score <= 0.1 && numAttempts > 8) return BURNED_OUT;
+        if (score <= 0.1 && numAttempts > 8) return FRUSTRATED;
         if (numAttempts > 0) return INITIATED;
         if (numAttempts == 0) return UNEXERCISED;
 
