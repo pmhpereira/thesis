6596c291dfb7f6f8dd322e5eab5efaa4278b9066
Pedro Miguel Henriques Pereira <pmh.pereira@gmail.com>
2016-03-03 11:42:57 +0000

    Added parallax

---
+++ Assets/Scripts/UI/Parallax.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    public float scrollSpeed;
+    public float tileSizeX;
+
+    private Vector3 startPosition, startParentPosition;
+
+    void Start ()
+    {
+        startPosition = transform.localPosition;
+		startParentPosition = transform.parent.localPosition;
+    }
+
+    void Update ()
+    {
+        float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSizeX);
+        transform.localPosition = startPosition + Vector3.right * newPosition;
+
+		transform.localPosition = new Vector3(transform.localPosition.x, -transform.parent.localPosition.y + startPosition.y, transform.localPosition.z);
+    }
+}
